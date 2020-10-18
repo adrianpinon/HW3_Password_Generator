172d05cc220bc4e470035dea5f4a1fe0282c5f0c
Adrian <adrian.pinon2329@gmail.com>
2020-10-17 20:37:41 -0500

    I spent hours trying to figure this thing out

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HW3_Password_Generator
+
+The Purpose of the assginment was to build this darn password generator.
+Now I gotta tell ya, this one was a doozy... Sooo it doesn't actually produce a password...
+However, i do understand the for/while loop itself. I just couldn't figure out how to actaully produce that darn password. I spent hours just staring at my screen. Eventully, I just hope and  prayed an  epiphany would occur and the code would just POP into my brain. I just didn't know how to apply it to a certain area so, I would  greatly appreciate any feedback you give me on how to fix my own.
+Please and Thank you!
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let upperCaseCharaters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let lowerCaseCharaters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let speicalCharaters = ["!", "?", "@", "#", "$", "%", "&", "*", ";"]
+let uppercaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let lowercaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let speicalCharacter = ["!", "?", "@", "#", "$", "%", "&", "*",";"]
 
-console.log(generateBtn)
+console.log(generateBtn);
 // Write password to the #password input
 function writePassword() {
+
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
@@ -15,20 +16,24 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-  let passwordLength = prompt("How Long do you want your password to be?")
-  let optionsArray = []
-  console.log(passwordLength)
-  if (passwordLength < 8 || passwordLength > 128) {
-    return ""
-  }
-  // ask the user a series of questions//
-  //1st question is do you want numbesr included in the password//
-  //2nd question is do you want speical charaters in the password//
-  //3rd question is do you want upper case charaters in the password//
-  //4rd question is do you want lower case charaters in the password//
-  //the questions must a confirm//
-  //loop through the password length//
+//loop through the password length//
+function generatePassword ()
+  {
+
+    passwordLength = prompt("How long do you want your password to be?                                 (Chose between 8 and 25 Characters)");
+    for (let passwordLength = 0[Math.floor(Math.random)]; passwordLength < password.Length; i++) 
+    if (passwordLength < 8 || passwordLength > 25)
+    
+
+    passwordNumber = confirm("D0 Y0u W4nt Numb3rs 1nclud3d 1n the 9assword?");
+        if (numbers) {generatePassword}
+    passwordSpeical = confirm("Do You W@nt $pe!cal Ch@r@cters !nclude !n the P@ssword?!");
+        if (speicalCharacter)  {generatePassword}
+    passwordUppercase = confirm("DO YOU WANT UPPERCASE CHARACTERS IN THE PASSWORD!");
+        if (uppercaseCharacter)  {generatePassword}
+    passwordLowercase = confirm("do you want lowercase characters in the password?");
+        if (lowercaseCharacter) {generatePassword}
+  
 }
 
 // Add event listener to generate button
